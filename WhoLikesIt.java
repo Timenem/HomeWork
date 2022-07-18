43cae266bb37ce26a1fd89ca6bbdd88f22cde342
Timenem <56394536+Timenem@users.noreply.github.com>
2022-07-18 00:33:00 +0000

    Create WhoLikesIt.java

---
+++ WhoLikesIt.java
@@ -0,0 +1,16 @@
+class WhoLikesIt {
+    public static String whoLikesIt(String... names) {
+        if (names.length == 0) {
+            return "no one likes this";
+        } else if (names.length == 1) {
+            return String.format("%s likes this", names[0]);
+        } else if (names.length == 2) {
+            return String.format("%s and %s likes this", names[0], names[1]);
+        } else if (names.length == 3) {
+            return String.format("%s, %s and %s like this", names[0], names[1], names[2]);
+        } else {
+            Integer others = names.length - 2;
+            return String.format("%s, %s and %d others like this", names[0], names[1], others);
+        }
+    }
+}
